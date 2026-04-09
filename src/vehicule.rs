f5c65e2fa3dba095522f8c65aec27db39a00cf56
eymeric <hatchchien@protonmail.com>
2026-04-09 12:15:50 +0200

    Change Vehicule.route type to Vec<String>

--- src/vehicule.rs
+++ src/vehicule.rs
@@ -1,5 +1,5 @@
 pub struct vehicule {
     capacity: u8,
     charge: u8, // charge of the vehicule at the moment, charge never exceed capacity
-    route: Vec<u8>, // client ids
+    route: Vec<String>, // client ids
 }
